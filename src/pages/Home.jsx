25560d68f928b9572eae8cc15da8c7d6d19973bb
unknown <bashabdulbash@gmail.com>
2023-01-05 01:35:00 +0100

    initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import UserResult from "../component/users/UserResults";
+import UserSearch from "../component/users/UserSearch";
+
+function Home() {
+  return (
+    <>
+      <UserSearch />
+      <UserResult />
+    </>
+  );
+}
+
+export default Home;
